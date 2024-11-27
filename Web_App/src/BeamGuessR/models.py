212256e2e48144241fb26b86fa6ea1887728aa3e
yann.champeau <Yann.CHAMPEAU@etu.uca.fr>
2024-11-27 17:21:48 +0100

    Added BeamGuessR models.:star:

--- Web_App/src/BeamGuessR/models.py
+++ Web_App/src/BeamGuessR/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from base64 import encode, decode
+
+class Map(models.Model):
+    name = models.CharField(max_length=50)
+    is_vanilla = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Guess(models.Model):
+    picture = models.BinaryField()
+    x_coord = models.FloatField()
+    y_coord = models.FloatField()
+    map = models.ForeignKey(Map, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return str(self.map) + " - " + str(self.x_coord) + ":" + str(self.y_coord)
+
+class User(models.Model):
+    #TODO User model
+    pass
